66f84a0c679fb10ffa6efaa1f4af237d0bce9a35
JoseCabrera88 <josecarloscabrera88@gmail.com>
2024-07-31 17:16:08 -0600

    Cuarto commit Suma con Manejo de Errores

--- CodigoVersionado5Veces.py
+++ CodigoVersionado5Veces.py
@@ -1,6 +1,9 @@
 def sumar(a, b):
     return a + b
 
-a = int(input("Ingresa el primer número: "))
-b = int(input("Ingresa el segundo número: "))
-print(sumar(a, b))
+try:
+    a = int(input("Ingresa el primer número: "))
+    b = int(input("Ingresa el segundo número: "))
+    print(sumar(a, b))
+except ValueError:
+    print("Por favor, ingresa números válidos.")
