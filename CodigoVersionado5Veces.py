f433920e08325f2ffcbfbee79c747ffb6e893c52
JoseCabrera88 <josecarloscabrera88@gmail.com>
2024-07-31 17:17:35 -0600

    Quinto commit Suma con Bucles para Múltiples Sumas

--- CodigoVersionado5Veces.py
+++ CodigoVersionado5Veces.py
@@ -1,9 +1,13 @@
 def sumar(a, b):
     return a + b
 
-try:
-    a = int(input("Ingresa el primer número: "))
-    b = int(input("Ingresa el segundo número: "))
-    print(sumar(a, b))
-except ValueError:
-    print("Por favor, ingresa números válidos.")
+while True:
+    try:
+        a = int(input("Ingresa el primer número: "))
+        b = int(input("Ingresa el segundo número: "))
+        print(f"La suma de {a} y {b} es {sumar(a, b)}")
+        continuar = input("¿Quieres sumar otros números? (sí/no): ")
+        if continuar.lower() != 'sí':
+            break
+    except ValueError:
+        print("Por favor, ingresa números válidos.")
